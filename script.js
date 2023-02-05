2e052c91935b55840e17cbcf6423890ab816cb45
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-02-05 20:27:45 +0100

    1 og 2 gjort

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 //Write your own code here
 //DON'T touch any other files!
+
+// document.body.innerHTML = document.body.innerHTML.replace(/<br>/gi, "");
+
+function removeBreaks() {
+  console.log("first assignment");
+  const allBreaks = document.querySelectorAll("br");
+  allBreaks.forEach((a) => {
+    a.remove();
+  });
+}
+
+removeBreaks();
+
+function addFilled() {
+  console.log("adding a class");
+  const allSecs = document.querySelectorAll("section");
+
+  allSecs.forEach((a) => {
+    a.classList.add("filled");
+  });
+}
+
+addFilled();
+
+function removeBefore() {
+  console.log("removing elements that comes before a sec");
+  const secs = document.querySelectorAll("section");
+  // do the rest
+}
+
+removeBefore();
